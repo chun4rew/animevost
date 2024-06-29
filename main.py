e3e1e7c7c5197ede1a9e13d7a7659a3932122f9f
chun4rew <achunarev@phoenixit.ru>
2024-06-29 18:26:07 +0300

    new feach

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ import wget
 import math
 import urllib.parse
 import subprocess
+from datetime import timedelta
+import os
+import whisperx
 
 '''
 Необходимые зависимости
@@ -39,6 +42,45 @@ headers = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
 
+
+def transcribe_video(input_video):
+    batch_size = 32
+    compute_type = "float32"
+    device = "cpu"
+
+    model = whisperx.load_model("large-v2", device=device, compute_type=compute_type)
+
+    audio = whisperx.load_audio(input_video)
+    result = model.transcribe(audio, batch_size=batch_size, language="en")
+
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+
+    segments = result["segments"]
+
+
+   # if srt file exists, delete it
+    if os.path.exists("subtitles.srt"):
+        os.remove("subtitles.srt")
+    for index, segment in enumerate(segments):
+        startTime = str(0)+str(timedelta(seconds=int(segment['start'])))+',000'
+        endTime = str(0)+str(timedelta(seconds=int(segment['end'])))+',000'
+        text = segment['text']
+        print(text)
+        segment = f"{index + 1}\n{startTime} --> {endTime}\n{text[1:] if text[0] == ' ' else text}\n\n"
+
+        srtFilename = os.path.join(f"subtitles.srt")
+        with open(srtFilename, 'a', encoding='utf-8') as srtFile:
+            srtFile.write(segment)
+
+    generator = lambda txt: TextClip(txt, font='Arial', fontsize=24, color='white')
+    subs = SubtitlesClip('subtitles.srt', generator)
+    subtitles = SubtitlesClip(subs, generator)
+
+    video = VideoFileClip(input_video)
+    result = CompositeVideoClip([video, subtitles.set_pos(('center', 'bottom'))])
+
+    result.write_videofile("output_subs.mp4")
 
 def decode_html_entities(page):
     def decode_html(match):
@@ -82,14 +124,14 @@ def process_video(input_video_path, output_video_path):
     # cv2.imwrite('output.jpg', cropped_img)
     background = ImageClip('test.jpg')
     start_time = 5 * 60
-    end_time = (6 * 60) - 10
+    end_time = 6 * 60
 
     # Trim the video using start_time and end_time values
     trimmed_video = video.subclip(start_time, end_time)
 
     # Loop the background video to match the duration of the trimmed video
     # background_loop = concatenate_videoclips([background] * math.ceil(trimmed_video.duration / background.duration))
-    background_loop = background.set_duration(trimmed_video.duration).resize((1080, 1524))
+    background_loop = background.set_duration(trimmed_video.duration).resize((1080, 1920))
 
 
     # Resize the trimmed video to fit the full length of the background
